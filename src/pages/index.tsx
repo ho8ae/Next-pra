ef4d3c4b4f3e8e89c499878cc6ee3fdf00c7e59c
ho8ae <xogh2242@gmail.com>
2024-09-01 17:33:04 +0900

    feat: UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import { ReactNode } from "react";
 import style from "./index.module.css";
 import SeacrhableLaout from "@/components/searchable-layout";
+import books from "@/mock/books.json"; //@ 는 src를 가리키는 것
+import BookItem from "@/components/book-item";
+
+
+
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}> 인덱스</h1>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book)=><BookItem key={book.id}{...book}/>)}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book)=><BookItem key={book.id}{...book}/>)}
+      </section>
+    </div>
+
   );
 }
 
