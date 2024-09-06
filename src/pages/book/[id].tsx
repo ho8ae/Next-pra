0331ad8553b95d6d419556fc60ee4e2b29d31d26
ho8ae <xogh2242@gmail.com>
2024-09-06 23:55:44 +0900

    fix: metaTag

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -31,7 +31,7 @@ export const getStaticPaths = () => {
     fallback: true,
     // false : 404 Notfound
     // blocking : SSR 방식
-    // true : SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
+    // true : SRR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
   };
 };
 
@@ -43,7 +43,7 @@ export const getStaticProps = async (
 
   if (!book) {
     return {
-      notFound: true, //자동으로 404page로 보내줌
+      notFound: true,
     };
   }
 
@@ -60,17 +60,20 @@ export default function Page({
   const router = useRouter();
 
   if (router.isFallback) {
-    return
-    <>
-    <Head>
-    <title>{title}</title>
-        <meta property="og:image" content={coverImgUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:desctription" content={description} />
-
-    </Head>
-      <div>loading</div>
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta
+            property="og:description"
+            content="한입 북스에 등록된 도서들을 만나보세요"
+          />
+        </Head>
+        <div>로딩중입니다</div>
       </>
+    );
   }
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
 
@@ -90,8 +93,7 @@ export default function Page({
         <title>{title}</title>
         <meta property="og:image" content={coverImgUrl} />
         <meta property="og:title" content={title} />
-        <meta property="og:desctription" content={description} />
-
+        <meta property="og:description" content={description} />
       </Head>
       <div className={style.container}>
         <div
