01247c7a5e1dd2bd018a31d49659914d9e92ecee
ho8ae <xogh2242@gmail.com>
2024-09-08 00:32:14 +0900

    page router or layout END

--- src/lib/fetch-books.ts
+++ src/lib/fetch-books.ts
@@ -3,7 +3,7 @@ import { BookData } from "@/types";
 export default async function fetchBooks(q?:string) : Promise<BookData[]> {
     //비동기는 반환 값이 Promise
     //? - 선택적 property
-    let url = `http://localhost:12345/book`
+    let url = `https://onebite-books-server-main-kappa.vercel.app/book`
 
     if(q){
         url += `/search?q=${q}`;
--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types";
 
 export default async function fetchOneBook(id:number):Promise<BookData | null>{
 //에러 시 null 값 반환되니까 union으로 타입 정의
-    const url = `http://localhost:12345/book/${id}`
+    const url = `https://onebite-books-server-main-kappa.vercel.app/book/${id}`
 
     try{
         const response = await fetch(url);
