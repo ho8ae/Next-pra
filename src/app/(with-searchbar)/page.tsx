926347e4a59b533d742ae45f7192efb74f96f4f5
ho8ae <xogh2242@gmail.com>
2024-09-11 22:35:47 +0900

    3.5 sever component 이해하기

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+// "use client"; //지시자를 명시하면 사용할 수 있음!
+
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log("home 컴포넌트 실행");
+
+    //REACT Hook은 브라우저에만 동작을 한다.
+  
   return (
     <div>
       인덱스페이지
